b26f227ef0c51bb2808c90e9b5125f45245c7d1e
Ray Lee <470466204@qq.com>
2020-11-17 17:17:14 -0800

    for test

--- hack/make-docker-images.sh
+++ hack/make-docker-images.sh
@@ -3,20 +3,23 @@ SCRIPTDIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
 log() { echo "$1" >&2; }
 
-TAG="${TAG:?TAG env variable must be specified}"
-REPO_PREFIX="${REPO_PREFIX:?REPO_PREFIX env variable must be specified}"
+#export TAG=v1
+#export REPO_PREFIX Ray
+#TAG="${TAG:?TAG env variable must be specified}"
+#REPO_PREFIX="${REPO_PREFIX:?REPO_PREFIX env variable must be specified}"
 
 while IFS= read -d $'\0' -r dir; do
     # build image
     svcname="$(basename "${dir}")"
-    image="${REPO_PREFIX}/$svcname:$TAG"
+    #image="${REPO_PREFIX}/$svcname:$TAG"
+    image="$svcname"
     (
         cd "${dir}"
         log "Building: ${image}"
         docker build -t "${image}" .
 
-        log "Pushing: ${image}"
-        docker push "${image}"
+        #log "Pushing: ${image}"
+        #docker push "${image}"
     )
 done < <(find "${SCRIPTDIR}/../src" -mindepth 1 -maxdepth 1 -type d -print0)
 
