fd183ed36b7d36546d88e05055b05c12c8885c3c
Ray Lee <470466204@qq.com>
2020-11-19 00:36:23 -0800

    add rollout for nodeserver

--- src/build.sh
+++ src/build.sh
@@ -1,6 +1,13 @@
 echo $1
 svcname="$1"
 cd $svcname
-image="192.168.1.62:5000/$svcname"
+if [ "$svcname" == "nodeserver" ]
+then
+    TAG=v2
+    image="192.168.1.62:5000/$svcname:$TAG"
+else
+    image="192.168.1.62:5000/$svcname"
+fi
+echo $image
 docker build -t "${image}" .
 docker push "${image}"
